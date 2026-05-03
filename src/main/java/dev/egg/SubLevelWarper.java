1c457fcfd88b8729a08f9f435f3a5f41c1eae0d8
Kai Burton <83238155+hollow-egg@users.noreply.github.com>
2026-05-03 05:36:08 -0700

    Added entity teleportation. Passengers and paintings currently do not work as expected

--- src/main/java/dev/egg/SubLevelWarper.java
+++ src/main/java/dev/egg/SubLevelWarper.java
@@ -3,6 +3,7 @@ package dev.egg;
 import com.ibm.icu.impl.Pair;
 import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
 import dev.egg.registries.BlockEntityRegistry;
+import dev.ryanhcode.sable.Sable;
 import dev.ryanhcode.sable.api.SubLevelHelper;
 import dev.ryanhcode.sable.api.sublevel.KinematicContraption;
 import dev.ryanhcode.sable.api.sublevel.ServerSubLevelContainer;
@@ -16,7 +17,10 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3d;
 
 import java.util.*;
@@ -52,10 +56,23 @@ public class SubLevelWarper {
         HashMap<UUID,Pair<UUID,Vec3i>> oldToNew = new HashMap<>();
         HashMap<UUID,CompoundTag> subLevelTags = new HashMap<>();
         HashMap<UUID,ServerLevelPlot> subLevelPlots = new HashMap<>();
+        HashMap<UUID,Set<Entity>> visitedEntities = new HashMap<>();
 
         for (SubLevel subLevel : compoundSubLevel) {
 
             ServerSubLevel serverSubLevel = (ServerSubLevel) subLevel;
+
+            //grab entities
+            var boxX = subLevel.boundingBox().width();
+            var boxY = subLevel.boundingBox().height();
+            var boxZ = subLevel.boundingBox().length();
+            AABB box = new AABB(-boxX/2 + center.x, -boxY/2 + center.y, -boxZ/2 + center.z, boxX/2 + center.x, boxY/2 + center.y, boxZ/2 + center.z);
+            box.inflate(1.0);
+
+            DimensionalSable.LOGGER.info(box.toString());
+
+            List<Entity> candidates = sourceContainer.getLevel().getEntities(null, box);
+            visitedEntities.put(subLevel.getUniqueId(), new HashSet<>(candidates));
 
             for(KinematicContraption contraption : serverSubLevel.getPlot().getContraptions())
                 ((AbstractContraptionEntity) contraption).disassemble();
@@ -78,12 +95,14 @@ public class SubLevelWarper {
             subLevelPlots.put(subLevel.getUniqueId(), copy.getPlot());
         }
 
+        Set<Entity> movedEntities = new HashSet<>();
         var physics = SubLevelPhysicsSystem.get(destinationContainer.getLevel());
         //load tags now that we have new plots and offsets
         for (SubLevel subLevel : compoundSubLevel) {
             ServerLevelPlot plot = subLevelPlots.get(subLevel.getUniqueId());
+            ServerSubLevel copy = plot.getSubLevel();
             //copy data to plot in other dimension
-            Pose3d pose = new Pose3d(plot.getSubLevel().logicalPose());
+            Pose3d pose = new Pose3d(copy.logicalPose());
             //modifies nbt data with custom block entity accessors
             SubLevelTemplate.load(plot, subLevelTags.get(subLevel.getUniqueId()),
                     new BlockEntityRegistry.MoveInfo(
@@ -92,10 +111,43 @@ public class SubLevelWarper {
                             sourceContainer.getLevel().dimension().location().getPath(),
                             destinationContainer.getLevel().dimension().location().getPath()));
 
-            physics.getPipeline().teleport(plot.getSubLevel(), pose.position(), pose.orientation());
+            physics.getPipeline().teleport(copy, pose.position(), pose.orientation());
 
             if (subLevel.getName() != null)
-                plot.getSubLevel().setName(subLevel.getName());
+                copy.setName(subLevel.getName());
+
+            //hopefully teleport entities
+            for(Entity entity : visitedEntities.get(subLevel.getUniqueId())) {
+                if (movedEntities.contains(entity))continue;
+                movedEntities.add(entity);
+
+                SubLevel tracking = Sable.HELPER.getTrackingOrVehicleSubLevel(entity);
+                DimensionalSable.LOGGER.info(entity.toString());
+
+//                if (tracking == subLevel) {
+//                    var centerBlock = ((ServerSubLevel)subLevel).getPlot().getCenterBlock();
+//                    var relativePos = entity.position().subtract(Vec3.atLowerCornerOf(centerBlock));
+//                    var newSubLevelPos = Vec3.atLowerCornerOf(copy.getPlot().getCenterBlock());
+//                    entity.teleportTo(copy.getLevel(),
+//                            newSubLevelPos.x + relativePos.x,
+//                            newSubLevelPos.y + relativePos.y,
+//                            newSubLevelPos.z + relativePos.z,
+//                            Set.of(),
+//                            entity.getYRot(),
+//                            entity.getXRot());
+//                }
+//                else {
+                    var offset = new Vector3d(position).sub(center);
+                    entity.teleportTo(plot.getSubLevel().getLevel(),
+                            entity.position().x + offset.x,
+                            entity.position().y + offset.y,
+                            entity.position().z + offset.z,
+                            Set.of(),
+                            entity.getYRot(),
+                            entity.getXRot());
+                //}
+                DimensionalSable.LOGGER.info(entity.toString());
+            }
         }
 
         //delete old sublevels
